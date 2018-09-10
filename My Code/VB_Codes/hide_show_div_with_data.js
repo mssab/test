d7b520c0d761864b3206ad00513b02a00ffaa0cb
Eng.Mussab Obeado <mssab1990@gmail.com>
2018-09-10 17:11:05 +0200

    Add files via upload

---
+++ My Code/VB_Codes/hide_show_div_with_data.js
@@ -0,0 +1,32 @@
+ <script type="text/javascript">
+        window.onload = function () {
+            
+            if (document.getElementById("ctl00_ContentPlaceHolder1_haendler2_name").innerText != "") {
+                document.getElementById("haendler2_box").style.display='block';
+            } else {
+                document.getElementById("haendler2_box").style.display='none';
+            }
+            if (document.getElementById("ctl00_ContentPlaceHolder1_haendler3_name").innerText != "") {
+                document.getElementById("haendler3_box").style.display='block';
+            } else {
+                document.getElementById("haendler3_box").style.display='none';
+            }
+
+            if (document.getElementById("ctl00_ContentPlaceHolder1_haendler4_name").innerText != "") {
+                document.getElementById("haendler4_box").style.display='block';
+            } else {
+                document.getElementById("haendler4_box").style.display='none';
+            }
+            if (document.getElementById("ctl00_ContentPlaceHolder1_haendler5_name").innerText != "") {
+                document.getElementById("haendler5_box").style.display='block';
+            } else {
+                document.getElementById("haendler5_box").style.display='none';
+            }
+            if (document.getElementById("ctl00_ContentPlaceHolder1_haendler6_name").innerText != "") {
+                document.getElementById("haendler6_box").style.display='block';
+            } else {
+                document.getElementById("haendler6_box").style.display='none';
+            }
+            
+        }
+    </script>
