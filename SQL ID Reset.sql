dc135c2e6b8acbd58a84e487f755d756096d0373
Eng.Mussab Obeado <mssab1990@gmail.com>
2018-10-16 17:40:36 +0200

    Update SQL ID Reset.sql

--- SQL ID Reset.sql
+++ SQL ID Reset.sql
@@ -1,3 +1,3 @@
 DBCC CHECKIDENT ('[Table]', RESEED, 0);
 
-CAST(DATEPART(year, [dbo].[sk_projekte].[va_beginn]) AS varchar) = 2018
+WHERE CAST(DATEPART(year, [dbo].[sk_projekte].[va_beginn]) AS varchar) = 2018 and prioritaet='1' order by sk_projekte.va_beginn
